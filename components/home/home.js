7fd3251d3ab1804039ac19c28a7d1c9de975d5e7
Audrey <alary.audrey4712@gmail.com>
2016-12-27 16:10:19 +0100

    ajout des cards

--- components/home/home.js
+++ components/home/home.js
@@ -1,8 +1,22 @@
 ((app)=>{
   'use strict'
   app.component('home',{
+    bindings:{
+      selectedArticle : "="
+    },
+    templateUrl :'components/home/home.html',
+  controller: function (){
+    this.articles=[{
+      title:"Val louron",
+      content:"differents pistes sont ouvertes sur le domaine"
+    },{
+      title:"Enfin les vacances",
+      content:"a l'occasion des vacances de noël, la station vous offre l'opportunité de gagner une paire de ski"
+    },{
+      title:"N'oubliez pas vos après ski",
+      content: "Temps pourri, la station tiens à vous informer que la route est glissante!"
 
-    templateUrl :'components/home/home.html'
-
-  })
+  }]
+  }
+})
 })(angular.module('app.home'))
